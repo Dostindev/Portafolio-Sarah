8b2824dfda51de5058e8a29f9c9feac131946f8b
Dostindev <Dostinsantana7@Hotmail.com>
2024-02-11 11:55:52 -0400

    ¨Portafolio¨

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+window.addEventListener("load", mostrarMensajeBienvenida);
+
+function mostrarMensajeBienvenida() {
+  const mensaje = "¡Bienvenido al Website Sarah Placencia!";
+  alert(mensaje);
+}
+
+let isWhite = true;
+
+    function toggleColor() {
+    const body = document.body;
+
+    if (isWhite) {
+        body.style.backgroundColor = "black";
+    } else {
+        body.style.backgroundColor = "white";
+    }
+
+    isWhite = !isWhite;
+}
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
